84e772a66fe3482a607fd1f0e17e859cb299219a
MatheusHubInfo <mm5426875@gmail.com>
2022-08-04 22:18:58 -0300

    estilização no css

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,11 @@
 var products = [
-  // {id: 1, name: 'Angular', description: 'Superheroic JavaScript MVW Framework.', price: 100},
-  // {id: 2, name: 'Ember', description: 'A framework for creating ambitious web applications.', price: 100},
-  // {id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100}
+  //  {id: 1, name: 'Angular', description: 'Superheroic JavaScript MVW Framework.', price: 100},
+  //  {id: 2, name: 'Ember', description: 'A framework for creating ambitious web applications.', price: 100},
+  //  {id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100}
 ];
 
 function findProduct (productId) {
+  console.log('teste de produto');
   return products[findProductKey(productId)];
 };
 
